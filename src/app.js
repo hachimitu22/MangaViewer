41080237d161bdbec51eacf80349ccdc4f8ed34e
hachimitu22 <hachimitu22.888@gmail.com>
2022-05-23 02:28:22 +0900

    charactersページを追加

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ const app = express();
 
 const topRouter = require('./routes/top')
 const tagsRouter = require('./routes/tags')
+const charactersRouter = require('./routes/characters')
 
 app.use('/', topRouter);
 app.use('/tags', tagsRouter);
+app.use('/characters', charactersRouter);
 
 module.exports = app;
