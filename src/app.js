d5b6982cc4c064d20a63e86c9f8466d7fa57207a
hachimitu22 <hachimitu22.888@gmail.com>
2022-05-23 02:56:48 +0900

    seriesページを追加

--- src/app.js
+++ src/app.js
@@ -4,9 +4,11 @@ const app = express();
 const topRouter = require('./routes/top')
 const tagsRouter = require('./routes/tags')
 const charactersRouter = require('./routes/characters')
+const seriesRouter = require('./routes/series')
 
 app.use('/', topRouter);
 app.use('/tags', tagsRouter);
 app.use('/characters', charactersRouter);
+app.use('/series', seriesRouter);
 
 module.exports = app;
