f1d45703652216b2b8fa54d46fb16705cdcfa6fd
hachimitu22 <hachimitu22.888@gmail.com>
2022-05-22 23:52:04 +0900

    tagsページを追加

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 const topRouter = require('./routes/top')
+const tagsRouter = require('./routes/tags')
 
 app.use('/', topRouter);
+app.use('/tags', tagsRouter);
 
 module.exports = app;
