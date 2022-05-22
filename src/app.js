c152c7691b14f94f732c08828047ec5e40f0f65f
hachimitu22 <hachimitu22.888@gmail.com>
2022-05-23 03:28:45 +0900

    artistページを追加

--- src/app.js
+++ src/app.js
@@ -5,10 +5,12 @@ const topRouter = require('./routes/top')
 const tagsRouter = require('./routes/tags')
 const charactersRouter = require('./routes/characters')
 const seriesRouter = require('./routes/series')
+const artistRouter = require('./routes/artist')
 
 app.use('/', topRouter);
 app.use('/tags', tagsRouter);
 app.use('/characters', charactersRouter);
 app.use('/series', seriesRouter);
+app.use('/artist', artistRouter);
 
 module.exports = app;
