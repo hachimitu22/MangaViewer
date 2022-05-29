579f9f96287555179a3815e76056bff0363033e0
hachimitu22 <hachimitu22.888@gmail.com>
2022-05-30 03:53:09 +0900

    unit test を出来るようにする

--- src/routes/top.js
+++ src/routes/top.js
@@ -1,8 +1,7 @@
 const express = require('express');
+const top = require('../handlers/top');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('top');
-});
+router.get('/', top);
 
 module.exports = router;
