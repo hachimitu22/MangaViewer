7918e71ca5d8c1e0376308982497ccdba4eb9938
hachimitu22 <hachimitu22.888@gmail.com>
2025-07-21 03:15:49 +0900

    アップロード画像をpublic/imagesに移動する

--- src/handlers/upload.js
+++ src/handlers/upload.js
@@ -1,10 +1,15 @@
 const multer = require('multer');
+const fs = require('fs');
 const path = require('path');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, '../../tmp'));
-    // cb(null, '/tmp');
+    const itemId = req.params && parseInt(req.params.id) || null;
+    const dir = path.join(__dirname, `../../tmp/${itemId}`);
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+    cb(null, dir);
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
@@ -16,6 +21,10 @@ const handlers = [
   upload.single('image'),
   (req, res) => {
     const itemId = req.params && parseInt(req.params.id) || null;
+    const filename = req.file.originalname;
+    const src = path.join(__dirname, `../../tmp/${itemId}/${filename}`);
+    const dest = path.join(__dirname, `../../public/images/${filename}`);
+    fs.renameSync(src, dest);
     res.redirect(`/edit/${itemId}`);
   },
 ];
