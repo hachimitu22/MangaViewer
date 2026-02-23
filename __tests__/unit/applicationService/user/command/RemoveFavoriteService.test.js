5af6c35c88d82d99e3f9608561b3dc96e2052a20
hachimitu22 <hachimitu22.888@gmail.com>
2026-02-24 01:07:49 +0900

    feat: RemoveFavoriteService

---
+++ __tests__/unit/applicationService/user/command/RemoveFavoriteService.test.js
@@ -0,0 +1,92 @@
+const {
+  Query,
+  FavoriteRemovedResult,
+  NotFoundUserResult,
+  NotAddedMediaResult,
+  RemoveFavoriteService: Service,
+} = require('../../../../../src/application/user/command/RemoveFavoriteService');
+const MediaId = require('../../../../../src/domain/media/mediaId');
+const User = require('../../../../../src/domain/user/user');
+const UserId = require('../../../../../src/domain/user/userId');
+// mock
+const MockUserRepository = require('../../__mocks__/MockUserRepository');
+
+describe('RemoveFavoriteService', () => {
+  let service;
+  let mockUserRepo;
+
+  beforeEach(() => {
+    mockUserRepo = new MockUserRepository();
+
+    service = new Service({
+      userRepository: mockUserRepo,
+    });
+  });
+
+  test('お気に入りからのメディア削除に成功した場合はお気に入り削除完了という結果が返却される', async () => {
+    // arrange
+    const user = new User(new UserId('uid'));
+    user.addFavorite(new MediaId('mid'));
+    mockUserRepo.findByUserId.mockResolvedValue(user);
+
+    // action
+    const result = await service.execute(new Query({ userId: 'uid', mediaId: 'mid' }));
+
+    // assert
+    expect(result).toEqual(new FavoriteRemovedResult());
+    expect(mockUserRepo.findByUserId).toHaveBeenCalledTimes(1);
+    expect(mockUserRepo.findByUserId).toHaveBeenCalledWith(new UserId('uid'));
+
+    expect(mockUserRepo.save).toHaveBeenCalledTimes(1);
+    expect(mockUserRepo.save).toHaveBeenCalledWith(new User(new UserId('uid')));
+  });
+
+  test('ユーザーが存在しない場合はユーザー未発見という結果となる', async () => {
+    // arrange
+    mockUserRepo.findByUserId.mockResolvedValue(null);
+
+    // action
+    const result = await service.execute(new Query({ userId: 'uid', mediaId: 'mid' }));
+
+    // assert
+    expect(result).toEqual(new NotFoundUserResult());
+    expect(mockUserRepo.findByUserId).toHaveBeenCalledTimes(1);
+    expect(mockUserRepo.findByUserId).toHaveBeenCalledWith(new UserId('uid'));
+
+    expect(mockUserRepo.save).toHaveBeenCalledTimes(0);
+  });
+
+  test('お気に入りしていないメディアを削除した場合はお気に入り不在という結果が返却される', async () => {
+    // arrange
+    mockUserRepo.findByUserId.mockResolvedValue(new User(new UserId('uid')));
+
+    // action
+    const result = await service.execute(new Query({ userId: 'uid', mediaId: 'mid' }));
+
+    // assert
+    expect(result).toEqual(new NotAddedMediaResult());
+    expect(mockUserRepo.findByUserId).toHaveBeenCalledTimes(1);
+    expect(mockUserRepo.findByUserId).toHaveBeenCalledWith(new UserId('uid'));
+
+    expect(mockUserRepo.save).toHaveBeenCalledTimes(0);
+  });
+
+  test('リポジトリの操作で例外が発生したらエラーとする', async () => {
+    // arrange
+    const user = new User(new UserId('uid'));
+    user.addFavorite(new MediaId('mid'));
+    mockUserRepo.findByUserId.mockResolvedValue(user);
+    mockUserRepo.save.mockRejectedValue(new Error("mockRepo error"));
+
+    // action
+    // assert
+    await expect(
+      service.execute(new Query({ userId: 'uid', mediaId: 'mid' }))
+    ).rejects.toThrow("mockRepo error");
+    expect(mockUserRepo.findByUserId).toHaveBeenCalledTimes(1);
+    expect(mockUserRepo.findByUserId).toHaveBeenCalledWith(new UserId('uid'));
+
+    expect(mockUserRepo.save).toHaveBeenCalledTimes(1);
+    expect(mockUserRepo.save).toHaveBeenCalledWith(new User(new UserId('uid')));
+  });
+});
