58723375173bff600a25b3a55f95ee46d6eef467
hachimitu22 <hachimitu22.888@gmail.com>
2026-03-12 00:29:06 +0900

    test: middleテスト用コマンドを追加

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,11 @@ module.exports = {
       testEnvironment: 'node',
     },
     {
+      displayName: 'middle',
+      testMatch: ['<rootDir>/__tests__/middle/**/*.test.js'],
+      testEnvironment: 'node',
+    },
+    {
       displayName: 'e2e',
       testMatch: ['<rootDir>/__tests__/e2e/**/*.test.js'],
       preset: "jest-puppeteer",
