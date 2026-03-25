a3f54389b9ae47038b015a4a87690ab29894b91c
hachimitu22 <hachimitu22.888@gmail.com>
2026-03-25 14:01:30 +0900

    fix: Puppeteer Pageのadd/removeListener互換をsetup前に補完
    CI ログの新しい失敗点は teardown ではなく setup 内 resetPage で、
    this.global.page.addListener が未定義なためテスト開始前に全スイートが停止していました。
    
    前回修正は teardown 対応が中心で、setup 時に呼ばれる addListener までは補完できていなかったため、
    原因箇所を再調査して Environment 初期化のより手前で互換パッチを当てるように変更しました。
    
    具体的には constructor で Page プロトタイプへ addListener/removeListener のエイリアスを設定し、
    setup/teardown でも page インスタンスに同等の補完を入れて、
    jest-environment-puppeteer が期待する旧 API を全ライフサイクルで満たすようにしています。

--- __tests__/large/e2e/support/PatchedPuppeteerEnvironment.js
+++ __tests__/large/e2e/support/PatchedPuppeteerEnvironment.js
@@ -2,7 +2,52 @@ const BasePuppeteerEnvironment = require('jest-environment-puppeteer')
 
 const PuppeteerEnvironment = BasePuppeteerEnvironment.default || BasePuppeteerEnvironment
 
+const patchPagePrototype = () => {
+  const packageNames = ['puppeteer', 'puppeteer-core']
+
+  packageNames.forEach(packageName => {
+    try {
+      const loaded = require(packageName)
+      const puppeteer = loaded.default || loaded
+      const PageClass = puppeteer && puppeteer.Page
+
+      if (!PageClass || !PageClass.prototype) {
+        return
+      }
+
+      if (typeof PageClass.prototype.addListener !== 'function' && typeof PageClass.prototype.on === 'function') {
+        PageClass.prototype.addListener = PageClass.prototype.on
+      }
+
+      if (typeof PageClass.prototype.removeListener !== 'function' && typeof PageClass.prototype.off === 'function') {
+        PageClass.prototype.removeListener = PageClass.prototype.off
+      }
+    } catch (_error) {
+      // ignore: package may not be resolvable in this execution context
+    }
+  })
+}
+
 class PatchedPuppeteerEnvironment extends PuppeteerEnvironment {
+  constructor(config, context) {
+    patchPagePrototype()
+    super(config, context)
+  }
+
+  async setup() {
+    await super.setup()
+
+    const page = this.global && this.global.page
+
+    if (page && typeof page.addListener !== 'function' && typeof page.on === 'function') {
+      page.addListener = page.on.bind(page)
+    }
+
+    if (page && typeof page.removeListener !== 'function' && typeof page.off === 'function') {
+      page.removeListener = page.off.bind(page)
+    }
+  }
+
   async teardown() {
     const page = this.global && this.global.page
 
