ba185a16c5788a38906d51c3b766444cc53f7b0b
hachimitu22 <hachimitu22.888@gmail.com>
2026-03-25 16:42:29 +0900

    fix: setup前にイベントAPI別名をObject.prototypeへ注入して互換化
    失敗ログを再精査すると、例外は super.setup 内 resetPage の時点で発生しており、
    page インスタンス生成後に補完する実装では到達前に落ちることが分かりました。
    
    また Puppeteer の Page クラス参照を経由する補完は、実行時に該当クラスへ辿れないケースがあり、
    addListener の未定義を確実に埋められていませんでした。
    
    そこで setup より前に Object.prototype へ非列挙の addListener/removeListener 別名を定義し、
    on/off（および addEventListener/removeEventListener）へ委譲する方式に変更しました。
    これにより jest-environment-puppeteer が super.setup/super.teardown で呼ぶ旧API前提を
    インスタンス生成前から一律で満たせるため、CI の初期化失敗を解消できます。

--- __tests__/large/e2e/support/PatchedPuppeteerEnvironment.js
+++ __tests__/large/e2e/support/PatchedPuppeteerEnvironment.js
@@ -2,60 +2,36 @@ const BasePuppeteerEnvironment = require('jest-environment-puppeteer')
 
 const PuppeteerEnvironment = BasePuppeteerEnvironment.default || BasePuppeteerEnvironment
 
-const patchPagePrototype = () => {
-  const packageNames = ['puppeteer', 'puppeteer-core']
+const defineAlias = (name, resolver) => {
+  if (typeof Object.prototype[name] === 'function') {
+    return
+  }
 
-  packageNames.forEach(packageName => {
-    try {
-      const loaded = require(packageName)
-      const puppeteer = loaded.default || loaded
-      const PageClass = puppeteer && puppeteer.Page
+  Object.defineProperty(Object.prototype, name, {
+    value: function alias(...args) {
+      const target = resolver(this)
 
-      if (!PageClass || !PageClass.prototype) {
-        return
+      if (typeof target === 'function') {
+        return target.apply(this, args)
       }
 
-      if (typeof PageClass.prototype.addListener !== 'function' && typeof PageClass.prototype.on === 'function') {
-        PageClass.prototype.addListener = PageClass.prototype.on
-      }
-
-      if (typeof PageClass.prototype.removeListener !== 'function' && typeof PageClass.prototype.off === 'function') {
-        PageClass.prototype.removeListener = PageClass.prototype.off
-      }
-    } catch (_error) {
-      // ignore: package may not be resolvable in this execution context
-    }
+      return this
+    },
+    configurable: true,
+    writable: true,
+    enumerable: false,
   })
+}
+
+const patchEventAliases = () => {
+  defineAlias('addListener', value => value.on || value.addEventListener)
+  defineAlias('removeListener', value => value.off || value.removeEventListener)
 }
 
 class PatchedPuppeteerEnvironment extends PuppeteerEnvironment {
   constructor(config, context) {
-    patchPagePrototype()
+    patchEventAliases()
     super(config, context)
-  }
-
-  async setup() {
-    await super.setup()
-
-    const page = this.global && this.global.page
-
-    if (page && typeof page.addListener !== 'function' && typeof page.on === 'function') {
-      page.addListener = page.on.bind(page)
-    }
-
-    if (page && typeof page.removeListener !== 'function' && typeof page.off === 'function') {
-      page.removeListener = page.off.bind(page)
-    }
-  }
-
-  async teardown() {
-    const page = this.global && this.global.page
-
-    if (page && typeof page.removeListener !== 'function' && typeof page.off === 'function') {
-      page.removeListener = page.off.bind(page)
-    }
-
-    await super.teardown()
   }
 }
 
